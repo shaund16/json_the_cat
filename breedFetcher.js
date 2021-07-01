bb6738e0d4b55d21b5ad47f8f4d64b21feb3bf77
Shaun <s-purslow@hotmail.com>
2021-07-01 01:48:19 +0000

    make breedFetcherTest file and add mocha tests to test if invalid breeds

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,21 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  if (error) return console.error(error);
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-  const [data] = JSON.parse(body);
-  if (!data) return console.log('Breed Not Found');
+    if (error) {
+      return callback(error);
+    }
+    const [data] = JSON.parse(body);
+    if (!data) {
+      return callback('Breed Not Found');
+    }
+    callback(null, data.description);
+  });
 
-  return console.log(data.description);
-});
+};
+
+module.exports = { fetchBreedDescription };
+
+
